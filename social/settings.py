51c2043e30955699c74158eb5b26e570fe77a8c9
pr1266 <pourya_1999@yahoo.com>
2020-08-03 00:12:02 -0700

    'postgres'

--- social/settings.py
+++ social/settings.py
@@ -96,9 +96,13 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pourya_social',
+        'USER': 'postgres',
+        'PASSWORD': '021021021021',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
