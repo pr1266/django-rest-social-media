7c434a0f73ae258b4dd7fc7b47ba3b89bb411231
pr1266 <pourya_1999@yahoo.com>
2020-08-02 22:17:44 -0700

    'urls'

--- social/urls.py
+++ social/urls.py
@@ -11,6 +11,34 @@ admin.site.index_title = 'Muscle Media Project Details'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #? inja ham faghat marboot be social medias:
+    #! Follow:
+    path('follow/', social_media_view.FollowListCreateView.as_view()),
+    path('follow/<int:pk>', social_media_view.FollowDetailUpdateDeleteAPIView.as_view()),
+    #! Post:
+    path('post/', social_media_view.PostListCreateView.as_view()),
+    path('post/<int:pk>', social_media_view.PostDetailUpdateDeleteAPIView.as_view()),
+    #! Like:
+    path('like/', social_media_view.LikeListCreateView.as_view()),
+    path('like/<int:pk>', social_media_view.LikeDetailUpdateDeleteAPIView.as_view()),
+    #! Comment:
+    path('comment/', social_media_view.CommentListCreateView.as_view()),
+    path('comment/<int:pk>', social_media_view.CommentDetailUpdateDeleteAPIView.as_view()),
+    #! CommentReply:
+    path('commentreply/', social_media_view.CommentReplyListCreateView.as_view()),
+    path('commentreply/<int:pk>', social_media_view.CommentReplyDetailUpdateDeleteAPIView.as_view()),
+    #! Hashtag:
+    path('hashtag/', social_media_view.HashtagListCreateView.as_view()),
+    path('hashtag/<int:pk>', social_media_view.HashtagDetailUpdateDeleteAPIView.as_view()),
+    #! HashtagPost:
+    path('hashtagpost/', social_media_view.HashtagPostListCreateView.as_view()),
+    path('hashtagpost/<int:pk>', social_media_view.HashtagPostDetailUpdateDeleteAPIView.as_view()),
+    #TODO:
+    #? for custom api views:
+    path('firstpage/', social_media_view.UserFirstPage.as_view()),
+    path('delete_like/', social_media_view.DeleteLikeOfPost.as_view()),
+    path('userprofilepage/<str:pk>/', social_media_view.UserProfilePage.as_view()),
+    path('postcomments/<int:pk>/', social_media_view.PostComments.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
